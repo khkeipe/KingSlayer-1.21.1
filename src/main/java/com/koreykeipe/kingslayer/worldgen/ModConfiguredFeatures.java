291beba758c2f21afbebdce64b9bce5d840ebb5f
khkeipe <kkeipe@gmail.com>
2025-11-24 17:05:33 -0700

    loot crate pile spawns with mix of regular blocks

--- src/main/java/com/koreykeipe/kingslayer/worldgen/ModConfiguredFeatures.java
+++ src/main/java/com/koreykeipe/kingslayer/worldgen/ModConfiguredFeatures.java
@@ -1,19 +1,45 @@
 package com.koreykeipe.kingslayer.worldgen;
 
 import com.koreykeipe.kingslayer.KingSlayer;
+import com.koreykeipe.kingslayer.block.ModBlocks;
+import net.minecraft.core.Holder;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstrapContext;
+import net.minecraft.data.worldgen.features.FeatureUtils;
+import net.minecraft.data.worldgen.placement.PlacementUtils;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.random.SimpleWeightedRandomList;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.SweetBerryBushBlock;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.feature.Feature;
-import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfiguration;
+import net.minecraft.world.level.levelgen.feature.configurations.*;
+import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvider;
+import net.minecraft.world.level.levelgen.feature.stateproviders.WeightedStateProvider;
+import net.minecraft.world.level.levelgen.placement.BiomeFilter;
+import net.minecraft.world.level.levelgen.placement.InSquarePlacement;
+import net.minecraft.world.level.levelgen.placement.PlacedFeature;
+import net.minecraft.world.level.levelgen.placement.RarityFilter;
+
+import java.util.List;
 
 public class ModConfiguredFeatures {
 
-
+    public static final ResourceKey<ConfiguredFeature<?,?>> CRATE_KEY = registerKey("crate");
 
     public static void bootstrap(BootstrapContext<ConfiguredFeature<?, ?>> context) {
+
+
+        register(context, CRATE_KEY,Feature.BLOCK_PILE,
+                new BlockPileConfiguration(
+                        new WeightedStateProvider(
+                                SimpleWeightedRandomList.<BlockState>builder()
+                                        .add(ModBlocks.SLAYER_CRATE.get().defaultBlockState(), 1)
+                                        .add(Blocks.GRAVEL.defaultBlockState(), 2)
+                                        .add(Blocks.COARSE_DIRT.defaultBlockState(), 5)
+                        )));
 
     }
 
--- src/main/java/com/koreykeipe/kingslayer/worldgen/ModPlacedFeatures.java
+++ src/main/java/com/koreykeipe/kingslayer/worldgen/ModPlacedFeatures.java
@@ -4,17 +4,23 @@ import com.koreykeipe.kingslayer.KingSlayer;
 import net.minecraft.core.Holder;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstrapContext;
+import net.minecraft.data.worldgen.placement.PlacementUtils;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
-import net.minecraft.world.level.levelgen.placement.PlacedFeature;
-import net.minecraft.world.level.levelgen.placement.PlacementModifier;
+import net.minecraft.world.level.levelgen.placement.*;
 
 import java.util.List;
 
 public class ModPlacedFeatures {
+
+    public static final ResourceKey<PlacedFeature>  CRATE_PLACED_KEY = registerKey("crate_placed");
+
     public static void bootstrap(BootstrapContext<PlacedFeature> context) {
         var configuredFeatures = context.lookup(Registries.CONFIGURED_FEATURE);
+
+        register(context,CRATE_PLACED_KEY, configuredFeatures.getOrThrow(ModConfiguredFeatures.CRATE_KEY),
+                List.of(RarityFilter.onAverageOnceEvery(8), InSquarePlacement.spread(), PlacementUtils.HEIGHTMAP_WORLD_SURFACE, BiomeFilter.biome()));
 
     }
 
