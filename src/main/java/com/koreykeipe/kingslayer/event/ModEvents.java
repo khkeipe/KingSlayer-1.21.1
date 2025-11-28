d62fdf9f5b04f5cb681a64bb540c388d2b7936b8
khkeipe <kkeipe@gmail.com>
2025-11-27 18:06:11 -0700

    changing welcome message to title

--- src/main/java/com/koreykeipe/kingslayer/event/ModEvents.java
+++ src/main/java/com/koreykeipe/kingslayer/event/ModEvents.java
@@ -2,14 +2,20 @@ package com.koreykeipe.kingslayer.event;
 
 import com.koreykeipe.kingslayer.KingSlayer;
 import com.koreykeipe.kingslayer.command.DeathCommand;
+import net.minecraft.ChatFormatting;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.protocol.game.ClientboundSetTitleTextPacket;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.stats.Stat;
 import net.minecraft.stats.Stats;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.GameType;
+import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.scores.*;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
@@ -22,19 +28,19 @@ public class ModEvents {
 
     @SubscribeEvent
     public static void onPlayerJoin(PlayerEvent.PlayerLoggedInEvent event){
-        if(event.getEntity() instanceof Player player){
+        if(event.getEntity() instanceof ServerPlayer player){
             CompoundTag persistent = player.getPersistentData();
             if(!persistent.contains("hasJoinedBefore")){
                 //First Time Join
                 persistent.putBoolean("hasJoinedBefore", true);
-                player.sendSystemMessage(Component.literal("Welcome to King Slayer!"));
-
+                player.connection.send(new ClientboundSetTitleTextPacket(Component.literal("Welcome to ")
+                        .append(Component.literal("King Slayer").withStyle(ChatFormatting.GOLD))
+                ));
                 Scoreboard scoreboard = player.getServer().getScoreboard();
                 PlayerTeam team = scoreboard.getPlayerTeam("aqua_team");
                 if(team != null){
                     scoreboard.addPlayerToTeam(player.getScoreboardName(), team);
                 }
-
             }else{
                 player.sendSystemMessage(Component.literal("Welcome Back o/"));
             }
@@ -62,36 +68,56 @@ public class ModEvents {
                 Scoreboard scoreboard = player.getServer().getScoreboard();
                 int deaths = player.getStats().getValue(Stats.CUSTOM, Stats.DEATHS);
                 int lives = MAX_DEATHS - deaths;
-                player.sendSystemMessage(Component.literal("You've Lost A Life, You Have " + lives + " Remaining"));
 
                 if(deaths == 0){
                     PlayerTeam team = scoreboard.getPlayerTeam("aqua_team");
                     if(team != null){
                         scoreboard.addPlayerToTeam(player.getScoreboardName(), team);
+                        player.sendSystemMessage(Component.literal("You Have ")
+                                .append(Component.literal(String.valueOf(lives)).withStyle(ChatFormatting.AQUA))
+                                .append(" Lives Remaining").withStyle(ChatFormatting.RESET)
+                        );
                     }
                 }
                 else if(deaths == 1){
                     PlayerTeam team = scoreboard.getPlayerTeam("green_team");
                     if(team != null){
                         scoreboard.addPlayerToTeam(player.getScoreboardName(), team);
+                        player.sendSystemMessage(Component.literal("You Have ")
+                                .append(Component.literal(String.valueOf(lives)).withStyle(ChatFormatting.DARK_GREEN))
+                                .append(" Lives Remaining").withStyle(ChatFormatting.RESET)
+                        );
                     }
                 }
                 else if(deaths == 2){
                     PlayerTeam team = scoreboard.getPlayerTeam("lime_team");
                     if(team != null){
                         scoreboard.addPlayerToTeam(player.getScoreboardName(), team);
+                        player.sendSystemMessage(Component.literal("You Have ")
+                                .append(Component.literal(String.valueOf(lives)).withStyle(ChatFormatting.GREEN))
+                                .append(" Lives Remaining").withStyle(ChatFormatting.RESET)
+                        );
                     }
                 }
                 else if(deaths == 3){
                     PlayerTeam team = scoreboard.getPlayerTeam("yello_team");
                     if(team != null){
                         scoreboard.addPlayerToTeam(player.getScoreboardName(), team);
+                        player.sendSystemMessage(Component.literal("You Have ")
+                                .append(Component.literal(String.valueOf(lives)).withStyle(ChatFormatting.YELLOW))
+                                .append(" Lives Remaining").withStyle(ChatFormatting.RESET)
+                        );
                     }
                 }
                 else if(deaths == 4){
                     PlayerTeam team = scoreboard.getPlayerTeam("red_team");
                     if(team != null){
                         scoreboard.addPlayerToTeam(player.getScoreboardName(), team);
+                        player.sendSystemMessage(Component.literal("You Have ")
+                                .append(Component.literal(String.valueOf(lives)).withStyle(ChatFormatting.RED))
+                                .append(" Life Remaining").withStyle(ChatFormatting.RESET)
+                                .append(". . . MAKE IT COUNT!").withStyle(ChatFormatting.DARK_RED)
+                        );
                     }
                 }
                 else if(deaths >= 5){
