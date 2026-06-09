de9793de8fcc7c868d7166dd638bec20c73a284b
kkeip <kkeipe@gmail.com>
2026-06-08 22:47:26 -0600

    working on loot tables.

--- src/main/java/com/koreykeipe/kingslayer/datagen/ModGlobalLootModifierProvider.java
+++ src/main/java/com/koreykeipe/kingslayer/datagen/ModGlobalLootModifierProvider.java
@@ -33,31 +33,124 @@ public class ModGlobalLootModifierProvider extends GlobalLootModifierProvider {
                 new AddItemModifier(new LootItemCondition[] {
                         LootItemBlockStatePropertyCondition.hasBlockStateProperties(ModBlocks.BROKEN_CRATE.get()).build(),
                         LootItemRandomChanceCondition.randomChance(.5f).build() }, ModItems.SLAYER_HOE.get()));
-        this.add("slayer_shovel_from_crate",
+        this.add("slayer_shovel_from_broken_crate",
                 new AddItemModifier(new LootItemCondition[] {
                         LootItemBlockStatePropertyCondition.hasBlockStateProperties(ModBlocks.BROKEN_CRATE.get()).build(),
                         LootItemRandomChanceCondition.randomChance(.5f).build() }, ModItems.SLAYER_SHOVEL.get()));
+        this.add("flint_from_broken_crate",
+                new AddItemModifier(new LootItemCondition[] {
+                        LootItemBlockStatePropertyCondition.hasBlockStateProperties(ModBlocks.BROKEN_CRATE.get()).build(),
+                        LootItemRandomChanceCondition.randomChance(.8f).build() }, Items.FLINT, 2, 5));
+        this.add("feather_from_broken_crate",
+                new AddItemModifier(new LootItemCondition[] {
+                        LootItemBlockStatePropertyCondition.hasBlockStateProperties(ModBlocks.BROKEN_CRATE.get()).build(),
+                        LootItemRandomChanceCondition.randomChance(.3f).build() }, Items.FEATHER, 1, 5));
+        this.add("carrot_from_broken_crate",
+                new AddItemModifier(new LootItemCondition[] {
+                        LootItemBlockStatePropertyCondition.hasBlockStateProperties(ModBlocks.BROKEN_CRATE.get()).build(),
+                        LootItemRandomChanceCondition.randomChance(.6f).build() }, Items.CARROT, 2, 5));
+        this.add("potato_from_broken_crate",
+                new AddItemModifier(new LootItemCondition[] {
+                        LootItemBlockStatePropertyCondition.hasBlockStateProperties(ModBlocks.BROKEN_CRATE.get()).build(),
+                        LootItemRandomChanceCondition.randomChance(.4f).build() }, Items.POTATO, 2, 5));
+        this.add("beat_from_broken_crate",
+                new AddItemModifier(new LootItemCondition[] {
+                        LootItemBlockStatePropertyCondition.hasBlockStateProperties(ModBlocks.BROKEN_CRATE.get()).build(),
+                        LootItemRandomChanceCondition.randomChance(.5f).build() }, Items.BEETROOT, 2, 5));
+        this.add("apple_from_broken_crate",
+                new AddItemModifier(new LootItemCondition[] {
+                        LootItemBlockStatePropertyCondition.hasBlockStateProperties(ModBlocks.BROKEN_CRATE.get()).build(),
+                        LootItemRandomChanceCondition.randomChance(.2f).build() }, Items.APPLE, 1, 3));
 
         // Common Crate Loot
-        this.add("slayer_pickaxe_from_crate",
+        this.add("slayer_pickaxe_from_common_crate",
                 new AddItemModifier(new LootItemCondition[] {
                         LootItemBlockStatePropertyCondition.hasBlockStateProperties(ModBlocks.COMMON_CRATE.get()).build(),
                         LootItemRandomChanceCondition.randomChance(.5f).build() }, ModItems.SLAYER_PICKAXE.get()));
+        this.add("raw_iron_from_common_crate",
+                new AddItemModifier(new LootItemCondition[] {
+                        LootItemBlockStatePropertyCondition.hasBlockStateProperties(ModBlocks.COMMON_CRATE.get()).build(),
+                        LootItemRandomChanceCondition.randomChance(.6f).build() }, Items.RAW_IRON, 1, 3));
+        this.add("raw_gold_from_common_crate",
+                new AddItemModifier(new LootItemCondition[] {
+                        LootItemBlockStatePropertyCondition.hasBlockStateProperties(ModBlocks.COMMON_CRATE.get()).build(),
+                        LootItemRandomChanceCondition.randomChance(.4f).build() }, Items.RAW_GOLD, 3, 8));
+        this.add("leather_from_common_crate",
+                new AddItemModifier(new LootItemCondition[] {
+                        LootItemBlockStatePropertyCondition.hasBlockStateProperties(ModBlocks.COMMON_CRATE.get()).build(),
+                        LootItemRandomChanceCondition.randomChance(.6f).build() }, Items.LEATHER, 1, 3));
+        this.add("bread_from_common_crate",
+                new AddItemModifier(new LootItemCondition[] {
+                        LootItemBlockStatePropertyCondition.hasBlockStateProperties(ModBlocks.COMMON_CRATE.get()).build(),
+                        LootItemRandomChanceCondition.randomChance(.4f).build() }, Items.BREAD, 2, 6));
+        this.add("fish_from_common_crate",
+                new AddItemModifier(new LootItemCondition[] {
+                        LootItemBlockStatePropertyCondition.hasBlockStateProperties(ModBlocks.COMMON_CRATE.get()).build(),
+                        LootItemRandomChanceCondition.randomChance(.5f).build() }, Items.COD, 2, 6));
+        this.add("stew_from_common_crate",
+                new AddItemModifier(new LootItemCondition[] {
+                        LootItemBlockStatePropertyCondition.hasBlockStateProperties(ModBlocks.COMMON_CRATE.get()).build(),
+                        LootItemRandomChanceCondition.randomChance(.6f).build() }, Items.MUSHROOM_STEW, 1, 3));
+        this.add("paper_from_common_crate",
+                new AddItemModifier(new LootItemCondition[] {
+                        LootItemBlockStatePropertyCondition.hasBlockStateProperties(ModBlocks.COMMON_CRATE.get()).build(),
+                        LootItemRandomChanceCondition.randomChance(.4f).build() }, Items.PAPER, 10, 30));
+
 
 
         // Rare Crate Loot
-        this.add("slayer_axe_from_crate",
+        this.add("slayer_sword_from_rare_crate",
                 new AddItemModifier(new LootItemCondition[] {
                         LootItemBlockStatePropertyCondition.hasBlockStateProperties(ModBlocks.RARE_CRATE.get()).build(),
-                        LootItemRandomChanceCondition.randomChance(.2f).build() }, ModItems.SLAYER_AXE.get()));
+                        LootItemRandomChanceCondition.randomChance(.2f).build() }, ModItems.SLAYER_SWORD.get()));
+        this.add("iron_from_rare_crate",
+                new AddItemModifier(new LootItemCondition[] {
+                        LootItemBlockStatePropertyCondition.hasBlockStateProperties(ModBlocks.RARE_CRATE.get()).build(),
+                        LootItemRandomChanceCondition.randomChance(.6f).build() }, Items.IRON_INGOT, 1, 5));
+        this.add("beef_from_rare_crate",
+                new AddItemModifier(new LootItemCondition[] {
+                        LootItemBlockStatePropertyCondition.hasBlockStateProperties(ModBlocks.RARE_CRATE.get()).build(),
+                        LootItemRandomChanceCondition.randomChance(.6f).build() }, Items.BEEF, 1, 5));
+        this.add("chicken_from_rare_crate",
+                new AddItemModifier(new LootItemCondition[] {
+                        LootItemBlockStatePropertyCondition.hasBlockStateProperties(ModBlocks.RARE_CRATE.get()).build(),
+                        LootItemRandomChanceCondition.randomChance(.6f).build() }, Items.CHICKEN, 1, 5));
+        this.add("apple_from_rare_crate",
+                new AddItemModifier(new LootItemCondition[] {
+                        LootItemBlockStatePropertyCondition.hasBlockStateProperties(ModBlocks.RARE_CRATE.get()).build(),
+                        LootItemRandomChanceCondition.randomChance(.6f).build() }, Items.APPLE, 1, 5));
+        this.add("gunpowder_rare_crate",
+                new AddItemModifier(new LootItemCondition[] {
+                        LootItemBlockStatePropertyCondition.hasBlockStateProperties(ModBlocks.RARE_CRATE.get()).build(),
+                        LootItemRandomChanceCondition.randomChance(.4f).build() }, Items.GUNPOWDER, 6, 12));
 
 
         // Epic Crate Loot
-        this.add("slayer_sword_from_crate",
+
+        this.add("slayer_axe_from_epic_crate",
                 new AddItemModifier(new LootItemCondition[] {
                         LootItemBlockStatePropertyCondition.hasBlockStateProperties(ModBlocks.EPIC_CRATE.get()).build(),
-                        LootItemRandomChanceCondition.randomChance(.2f).build() }, ModItems.SLAYER_SWORD.get()));
-
+                        LootItemRandomChanceCondition.randomChance(.2f).build() }, ModItems.SLAYER_AXE.get()));
+        this.add("cookies_from_epic_crate",
+                new AddItemModifier(new LootItemCondition[] {
+                        LootItemBlockStatePropertyCondition.hasBlockStateProperties(ModBlocks.EPIC_CRATE.get()).build(),
+                        LootItemRandomChanceCondition.randomChance(.8f).build() }, Items.COOKIE, 6, 16));
+        this.add("cake_from_epic_crate",
+                new AddItemModifier(new LootItemCondition[] {
+                        LootItemBlockStatePropertyCondition.hasBlockStateProperties(ModBlocks.EPIC_CRATE.get()).build(),
+                        LootItemRandomChanceCondition.randomChance(.4f).build() }, Items.CAKE, 2, 4));
+        this.add("pie_from_epic_crate",
+                new AddItemModifier(new LootItemCondition[] {
+                        LootItemBlockStatePropertyCondition.hasBlockStateProperties(ModBlocks.EPIC_CRATE.get()).build(),
+                        LootItemRandomChanceCondition.randomChance(.6f).build() }, Items.PUMPKIN_PIE, 3, 6));
+        this.add("diamonds_from_epic_crate",
+                new AddItemModifier(new LootItemCondition[] {
+                        LootItemBlockStatePropertyCondition.hasBlockStateProperties(ModBlocks.EPIC_CRATE.get()).build(),
+                        LootItemRandomChanceCondition.randomChance(.5f).build() }, Items.DIAMOND, 1, 6));
+        this.add("golden_apple_from_epic_crate",
+                new AddItemModifier(new LootItemCondition[] {
+                        LootItemBlockStatePropertyCondition.hasBlockStateProperties(ModBlocks.EPIC_CRATE.get()).build(),
+                        LootItemRandomChanceCondition.randomChance(.1f).build() }, Items.GOLDEN_APPLE, 1, 1));
 
 
         // Broken Airdrop Crate Loot
@@ -67,18 +160,34 @@ public class ModGlobalLootModifierProvider extends GlobalLootModifierProvider {
                 new AddItemModifier(new LootItemCondition[] {
                         LootItemBlockStatePropertyCondition.hasBlockStateProperties(ModBlocks.BROKEN_AD_CRATE.get()).build(),
                         LootItemRandomChanceCondition.randomChance(1f).build() }, Items.GOLDEN_APPLE, 0, 3));
+        this.add("wind_from_broken_airdop",
+                new AddItemModifier(new LootItemCondition[] {
+                        LootItemBlockStatePropertyCondition.hasBlockStateProperties(ModBlocks.BROKEN_AD_CRATE.get()).build(),
+                        LootItemRandomChanceCondition.randomChance(.6f).build() }, Items.WIND_CHARGE, 6, 16));
 
         // Common Airdrop Crate Loot
         this.add("golden_apple_from_common_airdop",
                 new AddItemModifier(new LootItemCondition[] {
                         LootItemBlockStatePropertyCondition.hasBlockStateProperties(ModBlocks.COMMON_AD_CRATE.get()).build(),
                         LootItemRandomChanceCondition.randomChance(1f).build() }, Items.GOLDEN_APPLE, 1, 2));
+        this.add("pearl_from_common_airdop",
+                new AddItemModifier(new LootItemCondition[] {
+                        LootItemBlockStatePropertyCondition.hasBlockStateProperties(ModBlocks.COMMON_AD_CRATE.get()).build(),
+                        LootItemRandomChanceCondition.randomChance(.6f).build() }, Items.ENDER_PEARL, 1, 3));
+        this.add("trident_from_common_airdop",
+                new AddItemModifier(new LootItemCondition[] {
+                        LootItemBlockStatePropertyCondition.hasBlockStateProperties(ModBlocks.COMMON_AD_CRATE.get()).build(),
+                        LootItemRandomChanceCondition.randomChance(.6f).build() }, Items.TRIDENT, 1, 1));
 
         // Rare Airdrop Crate Loot
         this.add("golden_apple_from_rare_airdop",
                 new AddItemModifier(new LootItemCondition[] {
                         LootItemBlockStatePropertyCondition.hasBlockStateProperties(ModBlocks.RARE_AD_CRATE.get()).build(),
                         LootItemRandomChanceCondition.randomChance(1f).build() }, Items.GOLDEN_APPLE, 1, 3));
+        this.add("mace_from_epic_airdop",
+                new AddItemModifier(new LootItemCondition[] {
+                        LootItemBlockStatePropertyCondition.hasBlockStateProperties(ModBlocks.RARE_AD_CRATE.get()).build(),
+                        LootItemRandomChanceCondition.randomChance(.6f).build() }, Items.MACE, 1, 1));
 
         //Epic Airdrop Crate Loot
         this.add("golden_apple_from_epic_airdop",
